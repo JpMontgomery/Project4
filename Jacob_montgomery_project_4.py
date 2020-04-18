3ecc4bbc418d1af8ad2d83d6a41bb245e60bf294
Jacob Montgomery <jacob.montgomery@mac.com>
2020-04-17 22:27:49 -0400

    Commit

--- Jacob_montgomery_project_4.py
+++ Jacob_montgomery_project_4.py
@@ -127,10 +127,12 @@ g = 9.8
 gps_std = 3
 gps_vel = .2
 acc_bias01 = [.25, .077, -.12]
+acc_bias0 = acc_bias01
 acc_mark = 0.0005 * g
 acc_T = 300
 acc_noise = .12 * g
 gyro_bias01 = [2.4 * 10 ** -4, -1.3 * 10 ** -4, 5.6 * 10 ** -4]
+gyro_bias0 = gyro_bias01
 gyro_mark = .3
 gyro_T = 300
 gyro_noise = .95
@@ -311,5 +313,5 @@ for time in INS_data.index:
             INS_data.loc[(time, 'g_b_z')] = gyro_bias0[2]
         old_time = time
         P_old = P_new
-        if time == 472.0:
-            break
+        # if time == 472.0:
+        #     break
